0ce7ac403168568d2a601231187ab13a57d04099
github username <github email address>
2018-10-04 18:21:33 -0400

    fixed modal news issue

--- app/models/load.rb
+++ app/models/load.rb
@@ -23,6 +23,8 @@ class Load < ApplicationRecord
 # makes a get request to a stock then updates or creates it in my backend
   def self.update_stock(symbol)
     stock = JSON.parse(RestClient.get"https://api.iextrading.com/1.0/stock/#{symbol}/batch?types=quote,news,chart")
-    Company.update_or_create_by(description: stock['quote']['sector'], price: stock['quote']['latestPrice'], name: stock['quote']['companyName'], symbol: stock['quote']['symbol'], high: stock['quote']['high'], low: stock['quote']['low'], open_price: stock['quote']['open'], close_price: stock['quote']['close'], news: stock['news'][0])
+    company = Company.update_or_create_by(description: stock['quote']['sector'], price: stock['quote']['latestPrice'], name: stock['quote']['companyName'], symbol: stock['quote']['symbol'], high: stock['quote']['high'], low: stock['quote']['low'], open_price: stock['quote']['open'], close_price: stock['quote']['close'], news: stock['news'][0])
+    massa = {company_id:company.id}.merge(stock['news'][0])
+    News.find_or_create_by(massa)
   end
 end
